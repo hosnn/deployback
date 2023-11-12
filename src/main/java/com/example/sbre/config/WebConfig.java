7ab61b8c00ea8970c527aafe01eb5dacee24316c
nae <soghtmd@naver.com>
2023-11-12 15:59:59 +0900

    kk

--- src/main/java/com/example/sbre/config/WebConfig.java
+++ src/main/java/com/example/sbre/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-//			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins("http://localhost:3000")
         	.allowedOrigins("https://testfn-dde8d.web.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE");
 //            .allowedHeaders("Content-Type", "Authorization")
